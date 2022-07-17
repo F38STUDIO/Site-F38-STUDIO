c706691e6ddfe2f9c746c1f8f1c8719b92544bd5
leGrandF38 <faycalyacoubaprog@gmail.com>
2022-07-17 13:01:11 +0200

    Teste liaison composants-pages (ok)

---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+
+
+function HomePage() {
+    return(
+        <div>
+            <Navbar/>
+            <h1>Home Page</h1>
+            <Footer/>
+            
+        </div>
+    );
+}
+
+export default HomePage;
