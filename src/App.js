e12d5d079ffae67689673a3e7129355f1e003938
leGrandF38 <faycalyacoubaprog@gmail.com>
2022-08-02 01:27:27 +0200

    routage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.scss';
-import HomePage from './pages/home';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/navbar';
+import HomeCatalogPage from './pages/HomeCatalogPage'
+import Cart from './pages/Cart'
+import CheckOut from './pages/CheckOut'
+import ProductDetailPage from './pages/ProductDetailPage'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navbar/>}>
+          <Route index element={<HomeCatalogPage />} >
+            <Route path='Product Detail' element={<ProductDetailPage/>}>
+              <Route path="Check Out" element={<CheckOut/>} />
+            </Route>
+          </Route>
+        <Route path="Cart" element={<Cart />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
